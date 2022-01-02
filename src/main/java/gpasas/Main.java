1b16b39bae68bcbccb587497c55990510ceb5646
Julius Vitkauskas <julius.vitkauskas@trafi.com>
2022-01-02 16:28:31 +0200

    Listen on 127.0.0.1 only

--- src/main/java/gpasas/Main.java
+++ src/main/java/gpasas/Main.java
@@ -64,7 +64,7 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        Javalin app = Javalin.create().start(8080);
+        Javalin app = Javalin.create().start("127.0.0.1", 8080);
 
         app.get("/", ctx -> {
             ctx.contentType("text/html");
